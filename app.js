44e09cc853e259e81a618a44ef5995380c4fca2d
harshpatil09 <139743913+harshpatil09@users.noreply.github.com>
2024-11-12 02:18:43 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
-const async = require('async'); 
+// Import the express module
+const express = require('express');
 
-async.parallel([ 
-	(callback) => { 
-		setTimeout(() => { 
-			console.log('This is the first function'); 
-			callback(null, 1); 
-		}, 500); 
-	}, 
-	(callback) => { 
-		console.log('This is the second function'); 
-		callback(null, 2); 
-	} 
-], (err, results) => { 
-	if (err) console.error(err); 
-	console.log(results); 
+// Create an Express application
+const app = express();
+
+// Set the view engine to Pug
+app.set('view engine', 'pug');
+
+// Set the views directory
+app.set('views', './views');
+
+// Define a route for the root URL
+app.get('/', (req, res) => {
+  // Render the "index" view and pass data to it
+  res.render('index', { title: 'Express Template', message: 'Express.js with template implementation!' });
+});
+
+// Start the server
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
